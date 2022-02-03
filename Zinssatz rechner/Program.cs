5f635d13e5ecf450893fcab4db91e9c1bb7c3930
Andi Sojeva <andi.s2@gso.schule.koeln>
2022-02-03 12:24:27 +0100

    Projektdateien hinzufügen.

---
+++ Zinssatz rechner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zinssatz_rechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
